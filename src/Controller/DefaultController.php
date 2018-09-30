4bd9979ef3c1eaaa98fd6548da98fb40000f0a9e
Youlich <jutatibouet@yahoo.fr>
2018-09-30 19:07:00 +0200

    Travail préparatoire : ajout de la page accueil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,17 +12,35 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render('index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('index.html.twig');
     }
 
 	/**
 	 * @Route("/services", name="services")
 	 */
     public function service(){
-	    return $this->render('services.html.twig', [
-		    'controller_name' => 'DefaultController',
-	    ]);
+	    return $this->render('services.html.twig');
     }
+
+	/**
+	 * @Route("/agency", name="agency")
+	 */
+	public function agency(){
+		return $this->render('agency.html.twig');
+	}
+
+	/**
+	 * @Route("/expertise", name="expertise")
+	 */
+	public function expertise(){
+		return $this->render('expertise.html.twig');
+	}
+
+	/**
+	 * @Route("/blog", name="blog")
+	 */
+	public function blog(){
+		return $this->render('blog.html.twig');
+	}
+
 }
